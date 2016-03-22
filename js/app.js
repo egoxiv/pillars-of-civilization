85eb6302bb005638fd50904fae36da8fa836fc2a
Erik Gomez <egoxiv@gmail.com>
2016-03-21 18:48:06 -0700

    Adds a start game function that gets players names, and a nextTurn function that adds data to player inventory and a counter that keeps track of current turn

--- js/app.js
+++ js/app.js
@@ -26,7 +26,6 @@ var Military = {
     var jets = this.jetFighters * 40;
     return soldiers + tanks + frigates + jets;
   }
-  // choices: [];
 };
 
 var Economy = {
@@ -61,25 +60,72 @@ var startGame = function() {
   player1Name = prompt('Player 1 enter your first name');
   player2Name = prompt('Player 2 enter your first name');
 
-  var $turnStatus = $('.turn-status');
-  $turnStatus.append(turn);
-
   $('.player-1 .player').text(player1Name);
   $('.player-2 .player').text(player2Name);
 
-  // Adds click event to NEXT button
+  // Adds click event to NEXT button after game starts
   $('.next-turn').on('click', function() {
     nextTurn(player1Name, player2Name);
   });
+
 };
 
 var nextTurn = function(player1, player2) {
+  // Updates dashboard
+  var p1Dashboard = $('.player-1');
+  var p2Dashboard = $ ('.player-2');
+
+  // Player 1 dashboard
+  p1Dashboard.children('.money').text(Economy.money);
+  p1Dashboard.children('.military').text( Economy.totalUnits() );
+
+  // Player 2 dashboard
+  p2Dashboard.children('.money').text(Military.money);
+  p2Dashboard.children('.military').text( Military.totalUnits() );
+
+
+  // Player 1 data setters
+  $('.player1-assets #empire').text( Economy.empire );
+  $('.player1-assets #money').text( Economy.money );
+  $('.player1-assets #population').text( Economy.population );
+  $('.player1-assets #soldiers').text( Economy.soldiers );
+  $('.player1-assets #tanks').text( Economy.tanks );
+  $('.player1-assets #frigates').text( Economy.frigates );
+  $('.player1-assets #jets').text( Economy.jetFighters );
+  $('.player1-assets #cashPerTurn').text( Economy.moneyPerTurn() );
+  $('.player1-assets #bill').text( Economy.bills );
+
+  // Player 2 data setters
+  $('.player2-assets #empire').text( Military.empire );
+  $('.player2-assets #money').text( Military.money );
+  $('.player2-assets #population').text( Military.population );
+  $('.player2-assets #soldiers').text( Military.soldiers );
+  $('.player2-assets #tanks').text( Military.tanks );
+  $('.player2-assets #frigates').text( Military.frigates );
+  $('.player2-assets #jets').text( Military.jetFighters );
+  $('.player2-assets #cashPerTurn').text( Military.moneyPerTurn() );
+  $('.player2-assets #bill').text( Military.bills );
+
+  var $turnStatus = $('.turn-status');
   turn++;
+  $turnStatus.text(turn);
 
-
+  if( turn === 3 ) {
+    console.log('First decision happens here');
+  } else if( turn === 6 ) {
+    console.log('Third decision goes here');
+  } else if ( turn === 9 ) {
+    console.log('Third decision goes here');
+  } else if ( turn === 12 ) {
+    console.log('Fourth decision goes here');
+  } else {
+    console.log(turn);
+  }
 
   Military.money += Military.moneyPerTurn();
   Economy.money += Economy.moneyPerTurn();
+
+
 
 };
 
