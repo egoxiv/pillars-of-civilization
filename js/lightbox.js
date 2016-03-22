07c7c57c4806f49b318aa7dcf278ea3b04e2ed48
Erik Gomez <egoxiv@gmail.com>
2016-03-21 21:16:22 -0700

    Added 2 more parameters to overlay function

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,24 +1,27 @@
 var overlay = function(heading, description, heading2, description2) {
 
 var $overlay      = $( "<div id='overlay'></div>" ),
-    $headerShell  = $("<h2>" + heading + "</h2>"),
-    $header2  = $("<h2>" + heading2 + "</h2>"),
-    $captionShell = $( "<p>" + description + "</p>" );
-    $caption2 = $( "<p>" + description2 + "</p>" );
+    $headerShell  = $("<input type=\'checkbox\'><h2>" + heading + "</h2>"),
+    $header2      = $("<input type=\'checkbox\'><h2>" + heading2 + "</h2>"),
+    $captionShell = $( "<p>" + description + "</p>" ),
+    $caption2     = $( "<p>" + description2 + "</p>" ),
+    $submit       = $("<input type=\'submit\' value=\'Submit\'>");
+
 
 $overlay.append( $headerShell );
 $overlay.append( $captionShell );
 
 $overlay.append( $header2 );
 $overlay.append( $caption2 );
+$overlay.append($submit);
 
 
 $( "body" ).append( $overlay );
 
 $overlay.show();
 
-$( $overlay ).click( function() {
-  $overlay.hide();
-});
+// $( $overlay ).click( function() {
+//   $overlay.hide();
+// });
 
 };
