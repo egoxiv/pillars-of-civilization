b7223cc6e53010a667bc4e29a78eceb2bae0d5ce
Erik Gomez <egoxiv@gmail.com>
2016-03-24 19:35:28 -0700

    Copyedits the decisions, and changes some of the math for more competitiveness.

--- js/decisions.js
+++ js/decisions.js
@@ -1,61 +1,63 @@
 var decisions = [
   {
-    headline1: 'Invest In Education',
-    description1: 'This decision will take your opponents population from the empire (200 citizens) and move them to your empire.',
+    headline1: 'Build a University',
+    description1: 'People from all over the world move to your empire to get an education at your new university. This decision will increase your total population by 200, which means you will make more gold per turn. Your opponent will lose 200 people and cash per turn.',
     actionA: function() {
       this.population = this.population + 200;
       this.opponent.population = this.opponent.population - 200;
     },
-    headline2: 'Steal Opponent\'s Tanks',
-    description2: 'Or you can choose to take away your opponents tanks (20) and add them to your inventory, reducing your opponents total unit damage and increasing yours.',
+    headline2: 'Steal Tanks!',
+    description2: 'You have people inside your opponents military that can steal your opponents tanks without being noticed! This decision will take 25% of your opponents tanks and park them in your military bases. Your opponents military will be overall weaker and smaller with this decision.',
     actionB: function() {
       this.tanks = this.tanks + 20;
       this.opponent.tanks = this.opponent.tanks - 20;
     }
   },
   {
-    headline1: 'Build Roads',
-    description1: 'Create jobs and take away your opponents citizens! This decision will increase your gold per turn, add 100 citizens to your total population and take 500 gold and 100 cictizens away from your opponents total.',
+    headline1: 'Build roads, bridges, and skyscrapers!',
+    description1: 'People are moving to your sprawling empire because there are so many jobs, more people means more cash per turn! This decision will take 300 people away from your opponent and decrease his cash per turn.',
     actionA: function() {
-      this.population = this.population + 100;
-      this.opponent.population = this.opponent.population - 100;
-      this.opponent.money = this.opponent.money - 500;
+      this.population = this.population + 300;
+      this.opponent.population = this.opponent.population - 300;
+      this.opponent.money = this.opponent.money - 1000;
     },
-    headline2: 'Recruit opponent\'s soldiers',
-    description2: 'Take 50 of your opponents soldiers and add them to your total soldier count, which will increase your total unit damage and reduce your opponents. Your opponent will be billed for the deserters for 500 gold.',
+    headline2: 'Take your opponents soldiers!',
+    description2: 'Your opponents soldiers are unhappy and are ready to leave the military to join yours. With this decision, you will have 50 more soldiers at the expense of your opponent, and your opponent will be billed for 1000.',
     actionB: function() {
       this.soldiers = this.soldiers + 50;
       this.opponent.soldiers = this.opponent.soldiers - 50;
-      this.opponent.bills = this.opponent.bills + 500;
+      this.opponent.bills = this.opponent.bills + 1000;
     }
   },
   {
     headline1: 'Extract Natural Resources',
-    description1: 'Make more gold per turn by adding 100 citizens, bill your opponent 1000 gold for expensive resources and take 100 citizens away.',
+    description1: 'Start extracting and selling you empires abundant natural resources and sell them to the world! Take 100 of your opponents people by creating jobs and bill him 1000 gold for your expensive resources.',
     actionA: function() {
       this.population = this.population + 100;
-      this.money = this.money + 1000;
+      this.money = this.money + 1500;
       this.opponent.population = this.opponent.population - 100;
-      this.opponent.bills = this.opponent.bills + 1000;
+      this.opponent.bills = this.opponent.bills + 1500;
     },
-    headline2: 'Invest in Military Research',
-    description2: 'Permanently increase the damage of all troops by increasing multiplier by 5, and discover new way to reduce your opponents military effectiveness.',
+    headline2: 'Modernize your military!',
+    description2: 'Replace all of your outdated military tech with all the new fancy stuff to increase your military\'s overall damage. Your opponents military will be less effective against you and reduce overall damage.',
     actionB: function() {
-      this.damageMultiplier = this.damageMultiplier + 10;
-      this.opponent.damageMultiplier = this.opponent.damageMultiplier - 10;
+      this.damageMultiplier = this.damageMultiplier + 5;
+      this.opponent.damageMultiplier = this.opponent.damageMultiplier - 5;
     }
   },
   {
-    headline1: 'Sanctions!',
-    description1: 'Sanction your opponent and charge him interest by reducing his gold per turn and sticking him with a 2000 bill. Get an automatic 2000 and make more gold per turn.',
+    headline1: 'Build the World Bank!',
+    description1: 'This decision will pay off all of your bills (if any), give you 2000 gold and increase your gold per turn. Your bank will force the opponent to pay your bills and reduce his gold per turn.',
     actionA: function() {
+      var playerBills = this.bills;
+      this.bills = 0;
       this.money = this.money + 2000;
       this.moneyDivider = this.moneyDivider - 2;
-      this.opponent.bills = this.opponent.bills + 2000;
+      this.opponent.bills = this.opponent.bills + playerBills;
       this.opponent.moneyDivider = this.opponent.moneyDivider + 2;
     },
-    headline2: 'Steal Military Base',
-    description2: 'Sneak up on your opponent and steal half of his fighter jets and frigates.',
+    headline2: 'Declare war first!',
+    description2: 'Your opponent will not see you coming which will allow you to steal half of his fighter jets and battleships to add to your total inventory!', // too powerful?
     actionB: function() {
       var fighterJets = this.opponent.jetFighters /2;
       var frigates = this.opponent.frigates / 2;
