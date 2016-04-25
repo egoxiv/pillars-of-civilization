aec6b294289ee1f6c4ae1bdddc1b566227be30b4
Erik Gomez <egoxiv@gmail.com>
2016-04-24 17:10:03 -0700

    Removes <button> elements from overlay and adds <a> tag with btn-danger class.

--- js/overlay.js
+++ js/overlay.js
@@ -1,17 +1,19 @@
 var decisionCount = 0;
 // **** BUG - btn-group not hiding when overlay is appended.
-// **** FIX - btn-group display set to none with .hide() before overlay is appended. btn-group is set to .show() after decisionCount reaches 2.
+// **** TEMPFIX - btn-group display set to none with .hide() before overlay is appended. btn-group is set to .show() after decisionCount reaches 2.
 
 var overlay = function(index, player) {
   var $overlay     = $('<div id="overlay"></div>');
   var $name      = $('<h1>' + player.name + '</h1>');
   var headline1    = $('<h3>' + decisions[index].headline1 + '</h3>');
   var description1 = $('<p>' + decisions[index].description1 + '</p>');
-  var $button1     = $('<button id="button1"></button');
+  // var $button1     = $('<button id="button1"></button');
+  var $button1     = $('<a class="btn btn-danger btn-sm" id="button1"></a>');
 
   var headline2    = $('<h3>' + decisions[index].headline2 + '</h3>');
   var description2 = $('<p>' + decisions[index].description2 + '</p>');
-  var $button2     = $('<button id="button2"></button');
+  // var $button2     = $('<button id="button2"></button');
+  var $button2     = $('<a class="btn btn-danger btn-sm " id="button2"></a>');
 
   $overlay.append($name);
   $overlay.append(headline1);
